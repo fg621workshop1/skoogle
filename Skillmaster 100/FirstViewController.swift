c7831fc5823365495921d01008311f22c2d9a39d
fg621workshop1 <fg621.workshop1@gmail.com>
2016-07-28 10:28:48 +0200

    update

--- Skillmaster 100/FirstViewController.swift
+++ Skillmaster 100/FirstViewController.swift
@@ -9,6 +9,71 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var Vorname: UITextField!
+    @IBOutlet weak var Nachname: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Skill: UITextField!
+    @IBOutlet weak var Speichernerfolgreich: UILabel!
+    @IBOutlet weak var Level: UISlider!
+    
+    @IBAction func zuruecksetzen(sender: AnyObject) {
+        
+        Vorname.text=""
+        Nachname.text=""
+        Email.text=""
+        Skill.text=""
+         Speichernerfolgreich.text=""
+        
+    }
+    var mySkillSpeichern = SkillSpeichernModel()
+    
+   // func textFieldShouldReturn(textField: UITextField) -> Bool {
+    //    Skill.resignFirstResponder()
+     //   return true
+   // }
+    
+   // override func viewDidLoad(){
+     //   super.viewDidLoad()
+      //  Skill.delegate = self
+       // Skill.text = bi
+   // }
+
+    
+    
+
+    @IBAction func TapGesture:(NSSet *)touches withEvent:(UIEvent *)event{
+        Speichernerfolgreich.text=""
+        UITouch *touch = [[event allTouches] anyObject];
+        if ([_textField isFirstResponder] && [touch view] != _textField) {
+            [_textField resignFirstResponder];
+        }
+        [super touchesBegan:touches withEvent:event];
+        
+    }
+    
+    @IBAction func Speichern(sender: AnyObject) {
+
+        if Email.text == "" {
+            Email.text="Emailadresse ungültig"
+        }
+        else{
+            
+        
+        let SpeichernErfolgreich: Bool = mySkillSpeichern.SkillSpeichern (Vorname.text! , Nachname: Nachname.text!, Email: Email.text!, SkillName: Skill.text!, Level: Int(Level.value))
+        
+        
+       //  let SpeichernErfolgreich: Bool = true
+        
+        if SpeichernErfolgreich {
+           // UIAlertAction (title: "Erfolg", style: UIAlertActionStyle, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+        Skill.text=""
+        Speichernerfolgreich.text="Skill erfasst"
+        }
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Skillmaster 100/SkillSpeichernModel.swift
@@ -0,0 +1,21 @@
+//
+//  SkillSpeichernModel.swift
+//  Skillmaster 100
+//
+//  Created by Grobel Kirsti, FG-621 on 27.07.16.
+//  Copyright © 2016 bmw. All rights reserved.
+//
+
+import UIKit
+
+class SkillSpeichernModel: NSObject {
+    
+    func SkillSpeichern (Vorname:String, Nachname:String, Email:String, SkillName:String, Level:Int) -> Bool{
+   
+ 
+        
+        return true
+        
+        }
+
+}
